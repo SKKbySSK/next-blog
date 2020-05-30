cf42aaa4a1b40f89f94f46a410fdec5c25b7aec5
Kaisei Sunaga <skkbyssk@gmail.com>
2020-05-31 00:32:24 +0900

    Remove unused apis

--- pages/api/pages.ts
+++
@@ -1,7 +0,0 @@
-import {IncomingMessage, ServerResponse} from "http";
-
-export default (req: IncomingMessage, res: ServerResponse) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ pages: [ { title: 'README', post: 'README.md' } ] }))
-}
